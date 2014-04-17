9817c97e964ca5f3a79ead78800887d7b2feeb48
Zsombor Papp <zsombor.papp@logicallabs.com>
2014-04-16 17:12:39 -0700

    Adding UI elements
    This commit adds all the UI elements of this simple app: table to
    show the list of devices, a button to refresh the list, a button to
    connect/disconnect a selected device, and a text field to enter
    a line of text that will be sent to the printer.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,46 @@
-function doClick(e) {
-    alert($.label.text);
+/*jslint white:true plusplus:true nomen:true vars:true sloppy:true undef:false bitwise:true*/
+/*global Alloy, $ */
+
+var
+	PrinterUtils = Alloy.Globals.PrintUtils,
+	devices, selectedDeviceIndex, selectedRow
+;
+
+function refreshDeviceList() {
+	var tableData;
+	
+	tableData = [];
+	devices = [];
+	
+	// TODO: Will need to acquire actual device list here.
+	tableData.push({
+		title: 'Fake Device For Testing GUI'
+	});
+	
+	$.deviceTable.setData(tableData);
+	
+	selectedDeviceIndex = -1;
+}
+
+function tableClick(e) {
+	if (selectedRow) {
+		selectedRow.hasCheck = false;
+	}
+	
+	selectedDeviceIndex = e.index;
+	selectedRow = e.row;
+	selectedRow.hasCheck = true;		
+}
+
+function toggleConnection() {
+	alert('Will connect/disconnect here; not ready yet!');
+}
+
+function print() {
+	alert('Will print here; not ready yet!');
+	$.textInput.value = '';
 }
 
 $.index.open();
+
+refreshDeviceList();
